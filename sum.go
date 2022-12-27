be03e3c119cc075b14a525c3ef24d04e6aa8f39c
conor.ney <conor.ux@gmail.com>
2022-12-26 23:46:44 -0800

    higher order function for fold/reducer

--- sum.go
+++ sum.go
@@ -1,1 +1,33 @@
 package main
+
+// Sum calculates the total from a slice of numbers.
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+// SumAllTails calculates the sums of all but the first number given a collection of slices.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+	var result = initialValue
+	for _, x := range collection {
+		result = accumulator(result, x)	
+	}
+	return result
+}
